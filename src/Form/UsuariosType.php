205507558a122d106c8dc857521ec3fa6f34bec7
nervaljunior <nerval.junior@discente.ufma.br>
2024-01-13 00:19:07 -0300

    resolvendo problema do formulario

--- src/Form/UsuariosType.php
+++ src/Form/UsuariosType.php
@@ -7,13 +7,23 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
 class UsuariosType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('email')
-            ->add('roles')
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'User' => 'ROLE_USER',
+                    'Admin' => 'ROLE_ADMIN',
+                    'nutricionista'=> 'ROLE_NUTRICIONISTA',
+                ],
+                'multiple' => true,
+                'expanded' => true,
+            ])
             ->add('password')
             ->add('nome')
             ->add('telefone')
@@ -21,7 +31,6 @@ class UsuariosType extends AbstractType
             ->add('isVerified')
         ;
     }
-
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
