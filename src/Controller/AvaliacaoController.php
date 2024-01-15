6ba0657e89b0f74fbe9a2da1cce59890f1791758
nervaljunior <nerval.junior@discente.ufma.br>
2024-01-14 22:10:29 -0300

    criação de anexos

--- src/Controller/AvaliacaoController.php
+++ src/Controller/AvaliacaoController.php
@@ -4,6 +4,11 @@ namespace App\Controller;
 
 use App\Entity\Avaliacao;
 use App\Form\AvaliacaoType;
+use App\Form\Anexo1Type;
+use App\Form\Anexo2Type;
+use App\Form\Anexo3Type;
+use App\Form\Anexo4Type;
+use App\Form\Anexo5Type;
 use App\Repository\AvaliacaoRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -64,6 +69,113 @@ class AvaliacaoController extends AbstractController
             'atividades' => $atividades,
             'mensagem' => $mensagem,
         ]);
+    }
+
+    #[Route('/anexo1', name: 'avaliacao_anexo1')]
+    public function anexo1(Request $request): Response
+    {
+        $avaliacao = new Avaliacao();
+        $form = $this->createForm(Anexo1Type::class, $avaliacao);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+
+            return $this->redirectToRoute('avaliacao_success', ['anexo' => 'Anexo 1']);
+        }
+
+        return $this->render('avaliacao/anexo1.html.twig', [
+            'form' => $form->createView(),
+            'anexo' => 'Anexo 1',
+        ]);
+    }
+    #[Route('/anexo2', name: 'avaliacao_anexo2')]
+    public function anexo2(Request $request): Response
+    {
+        $avaliacao = new Avaliacao();
+        $form = $this->createForm(Anexo2Type::class, $avaliacao);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Lógica para salvar a avaliação no banco de dados
+            // ...
+
+            return $this->redirectToRoute('avaliacao_success', ['anexo' => 'Anexo 2']);
+        }
+
+        return $this->render('avaliacao/anexo2.html.twig', [
+            'form' => $form->createView(),
+            'anexo' => 'Anexo 2',
+        ]);
+    }
+    
+    #[Route('/anexo3', name: 'avaliacao_anexo3')]
+    public function anexo3(Request $request): Response
+    {
+        $avaliacao = new Avaliacao();
+        $form = $this->createForm(Anexo3Type::class, $avaliacao);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Lógica para salvar a avaliação no banco de dados
+            // ...
+
+            return $this->redirectToRoute('avaliacao_success', ['anexo' => 'Anexo 3']);
+        }
+
+        return $this->render('avaliacao/anexo3.html.twig', [
+            'form' => $form->createView(),
+            'anexo' => 'Anexo 3',
+        ]);
+    }
+    #[Route('/anexo4', name: 'avaliacao_anexo4')]
+    public function anexo4(Request $request): Response
+    {
+        $avaliacao = new Avaliacao();
+        $form = $this->createForm(Anexo4Type::class, $avaliacao);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Lógica para salvar a avaliação no banco de dados
+            // ...
+
+            return $this->redirectToRoute('avaliacao_success', ['anexo' => 'Anexo 4']);
+        }
+
+        return $this->render('avaliacao/anexo4.html.twig', [
+            'form' => $form->createView(),
+            'anexo' => 'Anexo 4',
+        ]);
+    }
+    #[Route('/anexo5', name: 'avaliacao_anexo5')]
+    public function anexo5(Request $request): Response
+    {
+        $avaliacao = new Avaliacao();
+        $form = $this->createForm(Anexo5Type::class, $avaliacao);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Lógica para salvar a avaliação no banco de dados
+            // ...
+
+            return $this->redirectToRoute('avaliacao_success', ['anexo' => 'Anexo 5']);
+        }
+
+        return $this->render('avaliacao/anexo5.html.twig', [
+            'form' => $form->createView(),
+            'anexo' => 'Anexo 5',
+        ]);
+    }
+
+    #[Route('/avaliacao/success/{anexo}', name: 'avaliacao_success')]
+    public function success(string $anexo): Response
+    {
+        return $this->render('avaliacao/success.html.twig', ['anexo' => $anexo]);
     }
 
     private function calcularPontuacaoTotal($unidade, $pontuacoes)
---
+++ src/Form/Anexo1Type.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Avaliacao;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class Anexo1Type extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('DataAvaliacao')
+            ->add('nota')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Avaliacao::class,
+        ]);
+    }
+}
